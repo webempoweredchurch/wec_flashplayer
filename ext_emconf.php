0bc41d1039f4ee48966d7c875ab70b2d987428fe
Jeff Segars <jeff@webempoweredchurch.org>
2005-11-23 16:13:35 +0000

    Housekeeping stuff.  Added extension manual.  Removed changelog since it is tracked by subversion.  Update ext_emconf and Textmate project file.

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "wec_flashplayer"
 # 
-# Auto generated 22-11-2005 10:53
+# Auto generated 22-11-2005 13:26
 # 
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
@@ -18,8 +18,8 @@ $EM_CONF[$_EXTKEY] = Array (
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
-	'TYPO3_version' => '0.0.1-0.0.1',
-	'PHP_version' => '0.0.1-0.0.1',
+	'TYPO3_version' => '-',
+	'PHP_version' => '-',
 	'module' => '',
 	'state' => 'stable',
 	'internal' => 0,
@@ -36,7 +36,7 @@ $EM_CONF[$_EXTKEY] = Array (
 	'private' => 0,
 	'download_password' => '',
 	'version' => '1.0.1',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:27:{s:8:"ChangLog";s:4:"a5c2";s:27:"class.tx_wecflashplayer.php";s:4:"d2d8";s:12:"ext_icon.gif";s:4:"f772";s:17:"ext_localconf.php";s:4:"644f";s:14:"ext_tables.php";s:4:"e92d";s:28:"ext_typoscript_constants.txt";s:4:"a9e2";s:24:"ext_typoscript_setup.txt";s:4:"b8e5";s:24:"flash-ext_icon-sound.gif";s:4:"d476";s:21:"flexform_ds_audio.xml";s:4:"b33c";s:21:"flexform_ds_video.xml";s:4:"2c46";s:13:"locallang.php";s:4:"9241";s:16:"locallang_db.php";s:4:"3a22";s:22:"wec_flashplayer.tmproj";s:4:"b2c3";s:28:"wec_flashpresentation.tmproj";s:4:"8c34";s:19:"doc/wizard_form.dat";s:4:"e623";s:20:"doc/wizard_form.html";s:4:"3246";s:14:"pi1/ce_wiz.gif";s:4:"43fe";s:35:"pi1/class.tx_wecflashplayer_pi1.php";s:4:"486f";s:43:"pi1/class.tx_wecflashplayer_pi1_wizicon.php";s:4:"e157";s:17:"pi1/locallang.php";s:4:"d41d";s:14:"pi2/ce_wiz.gif";s:4:"9d69";s:35:"pi2/class.tx_wecflashplayer_pi2.php";s:4:"7269";s:43:"pi2/class.tx_wecflashplayer_pi2_wizicon.php";s:4:"4338";s:17:"pi2/locallang.php";s:4:"d41d";s:12:"res/100k.jpg";s:4:"e9cc";s:11:"res/mp3.swf";s:4:"9067";s:13:"res/video.swf";s:4:"f9a3";}',
+	'_md5_values_when_last_written' => 'a:26:{s:27:"class.tx_wecflashplayer.php";s:4:"4d24";s:12:"ext_icon.gif";s:4:"f772";s:17:"ext_localconf.php";s:4:"644f";s:14:"ext_tables.php";s:4:"e92d";s:28:"ext_typoscript_constants.txt";s:4:"a9e2";s:24:"ext_typoscript_setup.txt";s:4:"798b";s:24:"flash-ext_icon-sound.gif";s:4:"d476";s:21:"flexform_ds_audio.xml";s:4:"8425";s:21:"flexform_ds_video.xml";s:4:"2c46";s:13:"locallang.php";s:4:"9241";s:16:"locallang_db.php";s:4:"4ed4";s:22:"wec_flashplayer.tmproj";s:4:"1a5e";s:14:"doc/manual.sxw";s:4:"c85d";s:19:"doc/wizard_form.dat";s:4:"e623";s:20:"doc/wizard_form.html";s:4:"3246";s:14:"pi1/ce_wiz.gif";s:4:"43fe";s:35:"pi1/class.tx_wecflashplayer_pi1.php";s:4:"4baa";s:43:"pi1/class.tx_wecflashplayer_pi1_wizicon.php";s:4:"578d";s:17:"pi1/locallang.php";s:4:"d41d";s:14:"pi2/ce_wiz.gif";s:4:"9d69";s:35:"pi2/class.tx_wecflashplayer_pi2.php";s:4:"7c64";s:43:"pi2/class.tx_wecflashplayer_pi2_wizicon.php";s:4:"7a87";s:17:"pi2/locallang.php";s:4:"d41d";s:12:"res/100k.jpg";s:4:"e9cc";s:11:"res/mp3.swf";s:4:"68f6";s:13:"res/video.swf";s:4:"9458";}',
 );
 
 ?>
