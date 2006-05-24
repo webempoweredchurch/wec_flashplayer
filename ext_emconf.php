654a7b94cc3480ad3d2e20918814504b85951816
Jeff Segars <jeff@webempoweredchurch.org>
2006-05-24 20:45:06 +0000

    Automatic updates to ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,14 +2,15 @@
 
 ########################################################################
 # Extension Manager/Repository config file for ext: "wec_flashplayer"
-# 
-# Auto generated 06-12-2005 13:34
-# 
+#
+# Auto generated 24-05-2006 15:44
+#
 # Manual updates:
-# Only the data in the array - anything else is removed by next write
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
 ########################################################################
 
-$EM_CONF[$_EXTKEY] = Array (
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'WEC Flash Player',
 	'description' => 'Flash-based media player for MP3 audio or FLV video files.',
 	'category' => 'plugin',
@@ -18,8 +19,6 @@ $EM_CONF[$_EXTKEY] = Array (
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
-	'TYPO3_version' => '-',
-	'PHP_version' => '-',
 	'module' => '',
 	'state' => 'stable',
 	'internal' => 0,
@@ -33,10 +32,21 @@ $EM_CONF[$_EXTKEY] = Array (
 	'author_company' => 'Foundation for Evangelism',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'private' => 0,
-	'download_password' => '',
-	'version' => '1.1.0',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:26:{s:27:"class.tx_wecflashplayer.php";s:4:"a3ed";s:12:"ext_icon.gif";s:4:"f772";s:17:"ext_localconf.php";s:4:"644f";s:14:"ext_tables.php";s:4:"e92d";s:28:"ext_typoscript_constants.txt";s:4:"a9e2";s:24:"ext_typoscript_setup.txt";s:4:"798b";s:24:"flash-ext_icon-sound.gif";s:4:"d476";s:21:"flexform_ds_audio.xml";s:4:"1e03";s:21:"flexform_ds_video.xml";s:4:"360e";s:13:"locallang.php";s:4:"9241";s:16:"locallang_db.php";s:4:"4ed4";s:22:"wec_flashplayer.tmproj";s:4:"1a5e";s:14:"doc/manual.sxw";s:4:"815f";s:19:"doc/wizard_form.dat";s:4:"e623";s:20:"doc/wizard_form.html";s:4:"3246";s:14:"pi1/ce_wiz.gif";s:4:"43fe";s:35:"pi1/class.tx_wecflashplayer_pi1.php";s:4:"4baa";s:43:"pi1/class.tx_wecflashplayer_pi1_wizicon.php";s:4:"578d";s:17:"pi1/locallang.php";s:4:"d41d";s:14:"pi2/ce_wiz.gif";s:4:"9d69";s:35:"pi2/class.tx_wecflashplayer_pi2.php";s:4:"7c64";s:43:"pi2/class.tx_wecflashplayer_pi2_wizicon.php";s:4:"7a87";s:17:"pi2/locallang.php";s:4:"d41d";s:12:"res/100k.jpg";s:4:"e9cc";s:11:"res/mp3.swf";s:4:"4e95";s:13:"res/video.swf";s:4:"6f68";}',
+	'version' => '1.1.0',
+	'_md5_values_when_last_written' => 'a:26:{s:27:"class.tx_wecflashplayer.php";s:4:"763f";s:39:"class.tx_wecflashplayer_flashobject.php";s:4:"caee";s:12:"ext_icon.gif";s:4:"f772";s:17:"ext_localconf.php";s:4:"644f";s:14:"ext_tables.php";s:4:"e92d";s:28:"ext_typoscript_constants.txt";s:4:"a9e2";s:24:"ext_typoscript_setup.txt";s:4:"c94b";s:24:"flash-ext_icon-sound.gif";s:4:"d476";s:21:"flexform_ds_audio.xml";s:4:"1e03";s:21:"flexform_ds_video.xml";s:4:"cd01";s:13:"locallang.xml";s:4:"b253";s:16:"locallang_db.xml";s:4:"d559";s:22:"wec_flashplayer.tmproj";s:4:"1a5e";s:14:"doc/manual.sxw";s:4:"bbbe";s:14:"pi1/ce_wiz.gif";s:4:"43fe";s:35:"pi1/class.tx_wecflashplayer_pi1.php";s:4:"3ba2";s:43:"pi1/class.tx_wecflashplayer_pi1_wizicon.php";s:4:"1a4a";s:17:"pi1/locallang.xml";s:4:"fe8b";s:14:"pi2/ce_wiz.gif";s:4:"9d69";s:35:"pi2/class.tx_wecflashplayer_pi2.php";s:4:"a13a";s:43:"pi2/class.tx_wecflashplayer_pi2_wizicon.php";s:4:"2edc";s:17:"pi2/locallang.xml";s:4:"36b0";s:12:"res/100k.jpg";s:4:"e9cc";s:18:"res/flashobject.js";s:4:"9568";s:11:"res/mp3.swf";s:4:"4e95";s:13:"res/video.swf";s:4:"6f68";}',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+			'php' => '3.0.0-',
+			'typo3' => '3.5.0-',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
 );
 
 ?>
