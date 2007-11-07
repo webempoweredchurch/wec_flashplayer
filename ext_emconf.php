6359f95bbc42931ada858f02dd34a8b9225d293b
Jeff Segars <jeff@webempoweredchurch.org>
2007-11-07 22:14:07 +0000

    Updated emconf after release to TYPO3.org.

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "wec_flashplayer"
 #
-# Auto generated 08-12-2006 11:09
+# Auto generated 07-11-2007 12:04
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -33,12 +33,12 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'version' => '1.3.1',
-	'_md5_values_when_last_written' => 'a:27:{s:8:".project";s:4:"e6f1";s:27:"class.tx_wecflashplayer.php";s:4:"63f5";s:39:"class.tx_wecflashplayer_flashobject.php";s:4:"b4d3";s:12:"ext_icon.gif";s:4:"f772";s:17:"ext_localconf.php";s:4:"644f";s:14:"ext_tables.php";s:4:"e92d";s:28:"ext_typoscript_constants.txt";s:4:"a9e2";s:24:"ext_typoscript_setup.txt";s:4:"c94b";s:24:"flash-ext_icon-sound.gif";s:4:"d476";s:21:"flexform_ds_audio.xml";s:4:"1e03";s:21:"flexform_ds_video.xml";s:4:"cd01";s:13:"locallang.xml";s:4:"b253";s:16:"locallang_db.xml";s:4:"d559";s:22:"wec_flashplayer.tmproj";s:4:"1a5e";s:14:"doc/manual.sxw";s:4:"7131";s:14:"pi1/ce_wiz.gif";s:4:"43fe";s:35:"pi1/class.tx_wecflashplayer_pi1.php";s:4:"3ba2";s:43:"pi1/class.tx_wecflashplayer_pi1_wizicon.php";s:4:"1a4a";s:17:"pi1/locallang.xml";s:4:"fe8b";s:14:"pi2/ce_wiz.gif";s:4:"9d69";s:35:"pi2/class.tx_wecflashplayer_pi2.php";s:4:"a13a";s:43:"pi2/class.tx_wecflashplayer_pi2_wizicon.php";s:4:"2edc";s:17:"pi2/locallang.xml";s:4:"36b0";s:12:"res/100k.jpg";s:4:"e9cc";s:11:"res/mp3.swf";s:4:"4e95";s:16:"res/swfobject.js";s:4:"b168";s:13:"res/video.swf";s:4:"6f68";}',
+	'_md5_values_when_last_written' => 'a:25:{s:27:"class.tx_wecflashplayer.php";s:4:"72ac";s:39:"class.tx_wecflashplayer_flashobject.php";s:4:"5e08";s:12:"ext_icon.gif";s:4:"f772";s:17:"ext_localconf.php";s:4:"644f";s:14:"ext_tables.php";s:4:"e92d";s:28:"ext_typoscript_constants.txt";s:4:"a9e2";s:24:"ext_typoscript_setup.txt";s:4:"f5d7";s:24:"flash-ext_icon-sound.gif";s:4:"d476";s:21:"flexform_ds_audio.xml";s:4:"1e03";s:21:"flexform_ds_video.xml";s:4:"cd01";s:13:"locallang.xml";s:4:"b253";s:16:"locallang_db.xml";s:4:"d559";s:14:"doc/manual.sxw";s:4:"3dae";s:14:"pi1/ce_wiz.gif";s:4:"43fe";s:35:"pi1/class.tx_wecflashplayer_pi1.php";s:4:"3ba2";s:43:"pi1/class.tx_wecflashplayer_pi1_wizicon.php";s:4:"1a4a";s:17:"pi1/locallang.xml";s:4:"fe8b";s:14:"pi2/ce_wiz.gif";s:4:"9d69";s:35:"pi2/class.tx_wecflashplayer_pi2.php";s:4:"a13a";s:43:"pi2/class.tx_wecflashplayer_pi2_wizicon.php";s:4:"2edc";s:17:"pi2/locallang.xml";s:4:"36b0";s:12:"res/100k.jpg";s:4:"e9cc";s:11:"res/mp3.swf";s:4:"4e95";s:16:"res/swfobject.js";s:4:"66d4";s:13:"res/video.swf";s:4:"6f68";}',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
-			'php' => '3.0.0-',
-			'typo3' => '3.8.0-',
+			'php' => '3.0.0-0.0.0',
+			'typo3' => '3.8.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
