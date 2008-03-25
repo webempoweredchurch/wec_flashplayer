a01dee8254bd087f5f86e56f521b3c189c69376d
Jeff Segars <jeff@webempoweredchurch.org>
2008-03-25 02:32:29 +0000

    Configuration tweaks for new file locations and CSH support.

--- ext_tables.php
+++ ext_tables.php
@@ -10,14 +10,18 @@ $TCA["tt_content"]["types"]["list"]["subtypes_excludelist"][$_EXTKEY."_pi2"]="la
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi2']='pi_flexform';
 
 t3lib_extMgm::addPlugin(Array("LLL:EXT:wec_flashplayer/locallang_db.php:tt_content.list_type_pi1", $_EXTKEY."_pi1"),"list_type");
-t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:wec_flashplayer/flexform_ds_video.xml');
+t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:wec_flashplayer/pi1/flexform_ds_video.xml');
 
 t3lib_extMgm::addPlugin(Array("LLL:EXT:wec_flashplayer/locallang_db.php:tt_content.list_type_pi2", $_EXTKEY."_pi2"),"list_type");
-t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi2', 'FILE:EXT:wec_flashplayer/flexform_ds_audio.xml');
+t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi2', 'FILE:EXT:wec_flashplayer/pi2/flexform_ds_audio.xml');
 
 // Adds wizard icon to the content element wizard.
 if (TYPO3_MODE=='BE')	{
 	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_wecflashplayer_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_wecflashplayer_pi1_wizicon.php';
 	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_wecflashplayer_pi2_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'pi2/class.tx_wecflashplayer_pi2_wizicon.php';
 }
+
+/* Add Static templates */
+t3lib_extMgm::addStaticFile($_EXTKEY,"static", "WEC Flash Player");
+
 ?>
