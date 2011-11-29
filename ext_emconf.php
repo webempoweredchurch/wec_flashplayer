332df8a5fdbda2cac62756e40e3a534c4881876e
Jeff Segars <jsegars@alumni.rice.edu>
2011-11-29 13:59:35 -0600

    Release of version 1.4.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "wec_flashplayer"
+# Extension Manager/Repository config file for ext "wec_flashplayer".
 #
-# Auto generated 07-11-2007 12:04
+# Auto generated 29-11-2011 13:59
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -32,8 +32,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'Foundation for Evangelism',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '1.3.1',
-	'_md5_values_when_last_written' => 'a:25:{s:27:"class.tx_wecflashplayer.php";s:4:"72ac";s:39:"class.tx_wecflashplayer_flashobject.php";s:4:"5e08";s:12:"ext_icon.gif";s:4:"f772";s:17:"ext_localconf.php";s:4:"644f";s:14:"ext_tables.php";s:4:"e92d";s:28:"ext_typoscript_constants.txt";s:4:"a9e2";s:24:"ext_typoscript_setup.txt";s:4:"f5d7";s:24:"flash-ext_icon-sound.gif";s:4:"d476";s:21:"flexform_ds_audio.xml";s:4:"1e03";s:21:"flexform_ds_video.xml";s:4:"cd01";s:13:"locallang.xml";s:4:"b253";s:16:"locallang_db.xml";s:4:"d559";s:14:"doc/manual.sxw";s:4:"3dae";s:14:"pi1/ce_wiz.gif";s:4:"43fe";s:35:"pi1/class.tx_wecflashplayer_pi1.php";s:4:"3ba2";s:43:"pi1/class.tx_wecflashplayer_pi1_wizicon.php";s:4:"1a4a";s:17:"pi1/locallang.xml";s:4:"fe8b";s:14:"pi2/ce_wiz.gif";s:4:"9d69";s:35:"pi2/class.tx_wecflashplayer_pi2.php";s:4:"a13a";s:43:"pi2/class.tx_wecflashplayer_pi2_wizicon.php";s:4:"2edc";s:17:"pi2/locallang.xml";s:4:"36b0";s:12:"res/100k.jpg";s:4:"e9cc";s:11:"res/mp3.swf";s:4:"4e95";s:16:"res/swfobject.js";s:4:"66d4";s:13:"res/video.swf";s:4:"6f68";}',
+	'version' => '1.4.0',
+	'_md5_values_when_last_written' => 'a:30:{s:27:"class.tx_wecflashplayer.php";s:4:"ca18";s:39:"class.tx_wecflashplayer_flashobject.php";s:4:"1d8d";s:12:"ext_icon.gif";s:4:"f5ca";s:17:"ext_localconf.php";s:4:"644f";s:14:"ext_tables.php";s:4:"1c31";s:13:"locallang.xml";s:4:"b253";s:16:"locallang_db.xml";s:4:"d559";s:25:"csh/locallang_csh_pi1.xml";s:4:"9d3c";s:25:"csh/locallang_csh_pi2.xml";s:4:"663d";s:14:"doc/manual.sxw";s:4:"acba";s:14:"pi1/ce_wiz.gif";s:4:"bc63";s:35:"pi1/class.tx_wecflashplayer_pi1.php";s:4:"3ba2";s:43:"pi1/class.tx_wecflashplayer_pi1_wizicon.php";s:4:"1a4a";s:25:"pi1/flexform_ds_video.xml";s:4:"c10a";s:17:"pi1/locallang.xml";s:4:"1788";s:14:"pi2/ce_wiz.gif";s:4:"f303";s:35:"pi2/class.tx_wecflashplayer_pi2.php";s:4:"a13a";s:43:"pi2/class.tx_wecflashplayer_pi2_wizicon.php";s:4:"2edc";s:25:"pi2/flexform_ds_audio.xml";s:4:"c5f3";s:17:"pi2/locallang.xml";s:4:"853b";s:12:"res/100k.jpg";s:4:"e9cc";s:11:"res/mp3.swf";s:4:"4e95";s:16:"res/swfobject.js";s:4:"b58b";s:13:"res/video.swf";s:4:"6f68";s:11:"src/mp3.fla";s:4:"f274";s:11:"src/mp3.swf";s:4:"4e95";s:13:"src/video.fla";s:4:"f1fa";s:13:"src/video.swf";s:4:"6f68";s:20:"static/constants.txt";s:4:"a9e2";s:16:"static/setup.txt";s:4:"f5d7";}',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
